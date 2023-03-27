482ebee939c626ac706df49cf4b6e69c3e099406
扈炳儒 <554018447@qq.com>
2023-03-27 10:41:53 +0800

    测试

--- helper/measure.js
+++ helper/measure.js
@@ -3,7 +3,7 @@ import Overlay from 'ol/Overlay'
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
 import { LineString, Polygon } from 'ol/geom'
 // import { Vector as VectorSource } from 'ol/source'
-import { Vector as VectorLayer } from 'ol/layer'
+// import { Vector as VectorLayer } from 'ol/layer'
 import { getArea, getLength } from 'ol/sphere'
 import { unByKey } from 'ol/Observable'
 // import {transform} from 'ol/proj';
@@ -104,7 +104,7 @@ export function measure(map, type) {
     feature = null
     measureTooltipElement = null
     createMeasureTooltip(map)
-    // unByKey(listener)
+    unByKey(listener)
   })
 
   // 格式化长度
@@ -151,7 +151,7 @@ export function cancelMeasure(map) {
     helpTooltipElement.parentNode.removeChild(helpTooltipElement)
   }
   map.removeInteraction(draw)
-  // unByKey(mapMouseMove)
+  unByKey(mapMouseMove)
 }
 
 function createMeasureTooltip(map) {
