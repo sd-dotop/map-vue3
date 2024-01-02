12f03752198d282c7162831a0325870935ad8c6e
扈炳儒 <554018447@qq.com>
2024-01-02 10:49:32 +0800

    测试

--- helper/drawPolygon.js
+++ helper/drawPolygon.js
@@ -8,49 +8,60 @@
 // import { Vector as VectorSource } from 'ol/source';
 // import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
 // import { Polygon } from 'ol/geom';
+import Draw from 'ol/interaction/Draw'
+import Overlay from 'ol/Overlay'
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
+import { LineString, Polygon } from 'ol/geom'
+// import { Vector as VectorSource } from 'ol/source'
+// import { Vector as VectorLayer } from 'ol/layer'
+import VectorSource from 'ol/source/Vector'
+import VectorLayer from 'ol/layer/Vector'
+import { getArea, getLength } from 'ol/sphere'
+import { unByKey } from 'ol/Observable'
+// import {transform} from 'ol/proj';
 
-// var draw = null
-// var source = null
+let draw = null
+let source = null
 export function drawPolygon(map,color,color_,callback) {
-  // if(source){
-  //  source.clear()
-  //  map.removeInteraction(draw)
-  // }
-  // source = new VectorSource()
-  // const vector = new VectorLayer({
-  //   source: source,
-  //   style: new Style({
-  //     fill: new Fill({
-  //       color: color_ || 'rgba(255, 51, 51, 0.33)'
-  //     }),
-  //     stroke: new Stroke({
-  //       color: color || 'rgba(255, 51, 51, 1)',
-  //       width: 2
-  //     }),
-  //     image: new CircleStyle({
-  //       radius: 7,
-  //       fill: new Fill({
-  //         color: 'rgba(255, 51, 51, 0.33)'
-  //       })
-  //     })
-  //   })
-  // });
-  // map.addLayer(vector);
+  if(source){
+   source.clear()
+   map.removeInteraction(draw)
+  }
+  source = new VectorSource()
+  const vector = new VectorLayer({
+    source: source,
+    style: new Style({
+      fill: new Fill({
+        color: color_ || 'rgba(255, 51, 51, 0.33)'
+      }),
+      stroke: new Stroke({
+        color: color || 'rgba(255, 51, 51, 1)',
+        width: 2
+      }),
+      image: new CircleStyle({
+        radius: 7,
+        fill: new Fill({
+          color: 'rgba(255, 51, 51, 0.33)'
+        })
+      })
+    })
+  });
+  map.addLayer(vector);
  
-  // draw = new Draw({
-  //   source: source,
-  //   type: 'Polygon'
-  // });
-  // map.addInteraction(draw)
-  // draw.on("drawend", (evt)=> {
-  //   const geo = evt.feature.getGeometry();
-  //   const t = geo.getType();
-  //   if (t === "Polygon") {
-  //     // 获取坐标点
-  //     const points = geo.getCoordinates();
-  //     console.warn(points, "绘制结束，点坐标")
-  //     map.removeInteraction(draw); // 移除绘制
-  //     callback(points)
-  //   }
-  // });
+  draw = new Draw({
+    source: source,
+    type: 'Polygon'
+  });
+  map.addInteraction(draw)
+  draw.on("drawend", (evt)=> {
+    const geo = evt.feature.getGeometry();
+    const t = geo.getType();
+    if (t === "Polygon") {
+      // 获取坐标点
+      const points = geo.getCoordinates();
+      console.warn(points, "绘制结束，点坐标")
+      map.removeInteraction(draw); // 移除绘制
+      callback(points)
+    }
+  });
 }
